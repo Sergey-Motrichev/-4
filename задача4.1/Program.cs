ef72c4ab5785457253769d8894e8764a5880b63b
Sergey-Motrichev <112479065+Sergey-Motrichev@users.noreply.github.com>
2022-09-13 16:48:36 +0200

    решение4.1

---
+++ задача4.1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите цикл, который принимает на вход два числа
+//(А и В) и возводит число А в натуральную степень В.
+//3,5->245
+
+Console.Write("Введите число:");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число:");
+int b= Convert.ToInt32(Console.ReadLine());
+
+int Pow(int a){
+    int result =a;
+    for(int i=1;i<b;i++){
+        result *= a;
+    }
+    return result;
+}
+int result=Pow(a);
+System.Console.WriteLine($"{a}->{Pow(a)}");
