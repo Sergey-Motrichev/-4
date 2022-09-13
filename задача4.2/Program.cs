08c8335d181308f5ed55e456a73f51185c5c918b
Sergey-Motrichev <112479065+Sergey-Motrichev@users.noreply.github.com>
2022-09-13 17:17:28 +0200

    решение4.2

---
+++ задача4.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдает сумму
+//цифр в числе.
+
+Console.Write("Введите число:");
+int n=Convert.ToInt32(Console.ReadLine());
+
+    int sum=0;
+    while (n>0){
+        sum=sum+n%10;
+        n=n/10;
+    } 
+
+System.Console.WriteLine($" Сумма чисел {sum}");
